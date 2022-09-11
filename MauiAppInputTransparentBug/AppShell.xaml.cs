f649bb25f54877095bc66f3b86c6a484cc368022
yang zhou <xtuzhouyang@outlook.com>
2022-09-11 17:13:39 +0800

    添加项目文件。

---
+++ MauiAppInputTransparentBug/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiAppInputTransparentBug;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiAppInputTransparentBug/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+
+namespace MauiAppInputTransparentBug;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		PanGestureRecognizer panGestureRecognizer = new PanGestureRecognizer();
+		panGestureRecognizer.PanUpdated += (sender, e) =>
+		{
+#if WINDOWS
+			Debug.WriteLine($"Pan {e.StatusType.ToString()}");
+#else
+			Console.WriteLine($"Pan {e.StatusType.ToString()}");
+#endif
+            if (e.StatusType == GestureStatus.Completed||
+			e.StatusType == GestureStatus.Canceled)
+			{
+                //scrollView.InputTransparent = false;
+                //scrollView.CascadeInputTransparent = true;
+            }
+			else if(e.StatusType == GestureStatus.Started) 
+			{
+                scrollView.InputTransparent = true;
+                scrollView.CascadeInputTransparent = false;
+            }
+		};
+
+		rect.GestureRecognizers.Add(panGestureRecognizer);
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
